ca2579e42a4a6b4ae78077c5c1507b845434c10d
gventurini <giacomo.venturini3@gmail.com>
2023-05-14 17:26:48 +0200

    rework nca commit to work with nano interactive mode

--- git/commit-and-get-hash/commit-and-get-hash.js
+++ git/commit-and-get-hash/commit-and-get-hash.js
@@ -1,25 +1,21 @@
 module.exports = function (input) {
   const printSeparator = () => console.log('-'.repeat(50));
 
-  input.spawn('git', ['status'])
-    .then(() => {
-      printSeparator();
+  printSeparator();
 
-      const readline = require('readline');
-      const interface = readline.createInterface({ input: process.stdin, output: process.stdout });
-      const prompt = message => new Promise(resolve => interface.question(message, resolve));
+  input.spawnSync('git', ['status']);
 
-      return prompt('Insert your commit message: ');
-    })
-    .then(commitMessage => input.spawn('git', ['commit', '-m', `"${commitMessage}"`]))
-    .then(() => {
-      printSeparator();
-      return input.spawn('git', ['status']);
-    }).then(() => {
-      printSeparator();
-      input.shelljsSafeExec('git rev-parse --verify HEAD');
-      printSeparator();
-      process.exit(0);
-    }).catch(code => process.exit(code));
+  printSeparator();
 
+  input.spawnSync('git', ['commit'], { stdio: 'inherit' });
+
+  printSeparator();
+
+  input.spawnSync('git', ['status']);
+
+  printSeparator();
+
+  input.shelljsSafeExec('git rev-parse --verify HEAD');
+
+  printSeparator();
 };
