5033eb48cde6ad9551f5375c77de567f55b8fe3c
gventurini <giacomo.venturini3@gmail.com>
2024-03-22 18:24:54 +0100

    improve foreach by logging a separator before and after each output

--- foreach/foreach.js
+++ foreach/foreach.js
@@ -17,11 +17,24 @@ module.exports = function (input) {
     const childDir = path.join(workingDir, dir);
     process.chdir(childDir);
 
+    const executionMessage = `Executing command in ${childDir}`;
+    const separator = '-'.repeat(executionMessage.length);
+
+    const message = [
+      `\n\n${separator}`,
+      executionMessage,
+      `${separator}\n`
+    ].join('\n');
+
+    console.log(message);
+
     const exitCode = shelljs.exec(command).code;
 
     if (exitCode !== 0) {
       console.error(`Failed executing command '${command}' in ${process.cwd()} (exit code: ${exitCode})`);
     }
+
+    console.log(`\n${separator}`)
   });
 
 };
