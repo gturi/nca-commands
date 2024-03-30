dde90c7d105c8cf3b302799df434e38ee910bf28
gventurini <giacomo.venturini3@gmail.com>
2024-03-30 15:44:41 +0100

    add alias creation commands

--- setup.mjs
+++ setup.mjs
@@ -11,7 +11,17 @@ const __dirname = path.dirname(__filename);
 process.chdir(__dirname);
 
 function runCommand(command, ...args) {
-    spawnSync(command, args, { stdio: 'inherit' });
+    spawnSync(command, args, { stdio: 'inherit', shell: true });
+}
+
+function createNcaAlias(ncaCommand, aliasName = null) {
+    runCommand('nca', 'alias', 'add', ncaCommand);
+    if (aliasName) {
+        runCommand('nca', 'alias', 'rename', ncaCommand, aliasName);
+    }
 }
 
 runCommand('npm', 'link', 'node-command-alias');
+
+createNcaAlias('bookmark', 'bk');
+createNcaAlias('unbookmark', 'ubk');
