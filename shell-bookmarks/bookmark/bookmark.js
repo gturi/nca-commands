fe0c929fb839791eda6b120768b339597806ba23
gventurini <giacomo.venturini3@gmail.com>
2024-03-30 15:08:55 +0100

    add functions to make the create bookmark script clearer

--- shell-bookmarks/bookmark/bookmark.js
+++ shell-bookmarks/bookmark/bookmark.js
@@ -10,26 +10,48 @@ const ShellBookmarkCommon = require('../shell-bookmark-common.js');
  * @param {CommandHandlerInput} input
  */
 module.exports = function (input) {
-  const directory = StringUtils.isBlank(input.args.directory)
-    ? process.cwd()
-    : PathUtils.resolvePath(input.args.directory);
+  const directory = getTargetDirectory(input);
 
   const bookmarkName = getBookmarkName(input, directory);
 
   const bookmarkPath = ShellBookmarkCommon.getBookmarkPath(bookmarkName);
 
-  if (isWindows) {
-    const createShortcut = path.resolve(__dirname, 'create-shortcut.ps1');
-    runSync('powershell', '-c', createShortcut, directory, bookmarkPath);
-  } else {
-    runSync('ln', '-s', directory, bookmarkPath);
-  }
-};
+  createBookmark(directory, bookmarkPath);
 
+  console.log(`Created bookmark "${bookmarkName}" -> "${directory}"`);
+}
+
+/**
+ * @param {CommandHandlerInput} input
+ */
+function getTargetDirectory(input) {
+  return StringUtils.isBlank(input.args.directory)
+    ? process.cwd()
+    : PathUtils.resolvePath(input.args.directory);
+
+}
+/**
+ * @param {CommandHandlerInput} input
+ * @param {string} directory
+ */
 function getBookmarkName(input, directory) {
   let bookmarkName = StringUtils.isBlank(input.args.bookmarkName)
     ? path.basename(directory)
     : input.args.bookmarkName;
 
   return ShellBookmarkCommon.getSanitizedBookmarkName(bookmarkName);
+}
+
+/**
+ *
+ * @param {string} directory directory to bookmark
+ * @param {string} bookmarkPath path where the bookmark will be created at
+ */
+function createBookmark(directory, bookmarkPath) {
+  if (isWindows) {
+    const createShortcut = path.resolve(__dirname, 'create-shortcut.ps1');
+    runSync('powershell', '-c', createShortcut, directory, bookmarkPath);
+  } else {
+    runSync('ln', '-s', directory, bookmarkPath);
+  }
 }
