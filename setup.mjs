069ce8f6f27441ed087b7b87d0730640b1803442
gventurini <giacomo.venturini3@gmail.com>
2024-04-11 18:11:14 +0200

    update setup script with the new syntax for creating aliases

--- setup.mjs
+++ setup.mjs
@@ -10,18 +10,29 @@ const __dirname = path.dirname(__filename);
 
 process.chdir(__dirname);
 
+/**
+ *
+ * @param {string} command
+ * @param  {...string} args
+ */
 function runCommand(command, ...args) {
     spawnSync(command, args, { stdio: 'inherit', shell: true });
 }
 
+/**
+ * Create an alias for a command
+ *
+ * @param {string[]} ncaCommand - The command to create an alias for
+ * @param {string} [aliasName=null] - The name of the alias
+ */
 function createNcaAlias(ncaCommand, aliasName = null) {
-    runCommand('nca', 'alias', 'add', ncaCommand);
+    runCommand('nca', 'alias', 'add', ...ncaCommand);
     if (aliasName) {
-        runCommand('nca', 'alias', 'rename', ncaCommand, aliasName);
+        runCommand('nca', 'alias', 'rename', ncaCommand[0], aliasName);
     }
 }
 
 runCommand('npm', 'link', 'node-command-alias');
 
-createNcaAlias('bookmark', 'bk');
-createNcaAlias('unbookmark', 'ubk');
+createNcaAlias(['shell-bookmarks', 'bookmark'], 'bk');
+createNcaAlias(['shell-bookmarks', 'unbookmark'], 'ubk');
